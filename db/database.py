9324306c5beead564767af9254f36e79fab9c682
patmloi <patriciamloihy@gmail.com>
2022-03-03 00:18:42 +0800

    Update database.py
    Add notes on changing to PostgreSQL.

--- db/database.py
+++ db/database.py
@@ -3,11 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker 
 
 # Database URL: SQL Lite 
+# PostgreSQL: "postgresql://user:password@postgresserver/db"
 DbUrl = "sqlite:///./sql_app.db"
 
 # SQLALchemy engine 
 engine = create_engine(
-    DbUrl, connect_args={"check_same_thread": False}
+    DbUrl, connect_args={"check_same_thread": False} # check_same_thread not needed for PostgreSQL
 )
 
 
