df84560976852f53db127e6d783ac2b5556a5a56
patmloi <patriciamloihy@gmail.com>
2022-03-04 12:12:19 +0800

    Create password.py

---
+++ auth/password.py
@@ -0,0 +1,9 @@
+from passlib.context import CryptContext
+
+pwdContext = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def verifyPw(plainPw, hashedPw):
+    return pwdContext.verify(plainPw, hashedPw)
+
+def getHashedPw(plainPw: str) -> str:
+    return pwdContext.hash(plainPw)
